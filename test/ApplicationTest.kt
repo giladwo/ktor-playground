de2689b996b6edc1b196f479175763c07d9e762d
Gilad Woloch <25708271+giladwo@users.noreply.github.com>
2019-12-27 18:04:40 +0200

    Ignore example test

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -20,6 +20,7 @@ import kotlin.test.*
 import io.ktor.server.testing.*
 
 class ApplicationTest {
+    @Ignore
     @Test
     fun testRoot() {
         withTestApplication({ module(testing = true) }) {
